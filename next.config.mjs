0aae31703b6f739090eb474d67a3ade4cea1ef5c
Pranav.TJ <143486458+PranavTJ-05@users.noreply.github.com>
2024-03-26 23:36:40 +0530

    added few files

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        NEXT_PUBLIC_STRIPE_KEY: process.env.NEXT_PUBLIC_STRIPE_KEY,
+        STRIPE_SK_KEY: process.env.STRIPE_SK_KEY,
+        NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+        NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+        DATABASE_URL: process.env.DATABASE_URL,
+    },
+};
 
 export default nextConfig;
